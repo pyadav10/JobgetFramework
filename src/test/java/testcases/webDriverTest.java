7a009c440a4de70dc8a978f67d8cd59b2aa57485
Pankaj Office <pankaj.yadav@appinventiv.com>
2021-03-06 12:35:29 +0530

    First commit testing...

---
+++ src/test/java/testcases/webDriverTest.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class webDriverTest {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+
+	}
+
+}
